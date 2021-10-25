dc7938ef09c4e28c94b7e0ccc6f32f70a13a0cd8
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-10-25 15:32:30 +0530

    Counter Program - First solidity prog

---
+++ FirstSoldityCode-Counter.sol
@@ -0,0 +1,19 @@
+//my first solidity code
+
+pragma solidity ^0.6.0;
+
+contract Counter {
+    uint count;
+    
+    constructor() public{
+        count=0;
+    }
+    
+    function getCount() public view returns(uint){
+        return count;
+    }
+    
+    function incrementCount() public {
+        count=count+1;
+   }
+}
