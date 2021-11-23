c15fad8effc0f7a6c48b64d1ba6c722b837e4a61
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-11-23 14:22:03 +0530

    Hotel room - small project

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -1,33 +1,47 @@
-//Hotel Room Booking-Smart Contract
-
 pragma solidity ^0.6.0;
-
-contract HotelRoom {
-    
-    enum Statuses {Vacant, Occupied}
-    Statuses currentStatus;
-    address payable public owner;
-    
-    event Occupy(address _occupant, uint _value);
-    
-    constructor() public{
-        owner=msg.sender;
-        currentStatus = Statuses.Vacant;
-    }
-    
-    modifier onlyWhileVacant {
-        require(currentStatus==Statuses.Vacant, "Already Occupied!");
-        _;
-    }
-    
-    modifier costs(uint _amount) {
-        require(msg.value >= _amount, "Not enough Ether Provided!");
-        _;
-    }
-    
-    receive() external payable onlyWhileVacant costs(2 ether) {
-        currentStatus=Statuses.Occupied;
-        owner.transfer(msg.value);
-        emit Occupy(msg.sender, msg.value);
-    }
+contract HotelRoom1 {
+//Project - Hotel Room Booking    
+//THINGS TO LEARN FROM THIS CODE CRYPTOCURRENCY PAYMENTS:- 
+ //Ether - Payment in smart contracts
+ //Modifiers
+ //require() - ensure the conditions before the code gets executed
+ //Visibility Modes 
+ //Events 
+ //Enums - Enumerations
+ 
+ enum Statuses {Vaccant, Occupied}
+ Statuses currentStatus;
+ address payable public owner; 
+ //owner will be the one who dynamically deploys the smart contact 
+ //payable - cause owner can accept the payments in ethers
+ event Occupy (address _occupant, uint _value);
+  
+ constructor() public{
+     owner=msg.sender;
+     //msg.sender - user that calls the smart contract is the owner
+     currentStatus = Statuses.Vaccant;
+ }
+ 
+ modifier onlyWhileVacant {
+     require(currentStatus==Statuses.Vaccant, "Currently Occupied!!");
+     _;
+    //require checks the condition-if true then proceed with the code flow
+    //else- display the error message and halt the flow the code. 
+ }
+ 
+ modifier costs(uint _amount) {
+     require(msg.value >= _amount, "Not enough Ether Provided!!");
+     _;
+ }
+ 
+ receive() external payable onlyWhileVacant costs(2 ether) {
+    //enternal - called as a smart contract function outside 
+    //check price and status
+    currentStatus=Statuses.Occupied;
+    owner.transfer(msg.value);
+    //msg.value - for getting the ethers value from the sender account to owner
+    emit Occupy(msg.sender, msg.value);
+    //to emit the event(Occupy) created above - address and amount(ether paid)
+ }
+ 
 }
